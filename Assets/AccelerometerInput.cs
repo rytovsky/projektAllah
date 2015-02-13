51788f07f1d321653f0781ab12262ee186c8f289
rytovsky <rytu.official@gmail.com>
2015-02-13 13:46:31 +0100

    134

---
+++ Assets/AccelerometerInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelerometerInput : MonoBehaviour {
+
+
+	void Update () 
+	{
+		if (transform.position.y < 10)
+			transform.localPosition = new Vector3(transform.position.x,1,transform.position.z);
+		else
+			transform.Translate(Input.acceleration.x*Time.deltaTime*10, 0,Input.acceleration.y *Time.deltaTime*10);
+	}
+}
